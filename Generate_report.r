598a6d0cd864d8b9840002a8577ac1b97152663c
DanielThedie <dthedie@gmail.com>
2025-02-03 10:02:45 +0000

    Renamed graph to interactions for disambiguation

--- Generate_report.r
+++ Generate_report.r
@@ -1,25 +1,14 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-# library("rmarkdown")
-=======
 library(igraph)
 library(intergraph)
 library(statnet)
->>>>>>> 3bab69e29d63a8e1997e5b264d97a9d0d144eb9a
-=======
-library(igraph)
-library(intergraph)
-library(statnet)
->>>>>>> 3bab69e29d63a8e1997e5b264d97a9d0d144eb9a
 
 source("src/Report_class.r")
 source("src/Report_text.r")
-source("src/Graph_class.r")
+source("src/Interactions_class.r")
 
 # Load data
 graph_path <- "E:\\Daniel\\Network-Analysis\\data\\SBS_Direct_Andrew_Data.net"
-institution <- Graph(file_path = graph_path)
+institution <- Interactions(file_path = graph_path)
 
 # Initiate report
 report <- Report()
---
+++ SRC/Interactions_class.r
@@ -0,0 +1,108 @@
+library(dplyr)
+library(tidyverse)
+
+Interactions <- function(file_path) {
+  graph <- load_graph(file_path)
+  network <- asNetwork(graph)
+  interactions <- list(file_path = file_path, graph = graph, network = network)
+
+  # Assign the class name
+  class(interactions) <- "Interactions"
+
+  return(interactions)
+}
+
+load_graph <- function(file_path) {
+  if (!file.exists(file_path)) {
+    stop("File not found.")
+  } else {
+    graph <- read_graph(file_path, format = "pajek")
+  }
+  return(graph)
+}
+
+make_graph_from_csv <- function(file_path, delimiter = ";", column_name = "Author") {
+  data <- read.csv(file_path, stringsAsFactor = FALSE)
+
+  # Create edges: pairwise combinations of co-authors
+  edges <- data %>%
+    mutate(Authors = str_split(Author, delimiter)) %>%
+    filter(map_int(Authors, length) > 1) %>%  # Remove papers with less than 2 authors (no interaction can be inferred from them)
+    mutate(pairs = map(strsplit(Author, delimiter), ~combn(.x, 2, simplify = FALSE))) %>%  # Generate all pairs of authors
+    unnest(pairs) %>%
+    unnest_wider(pairs, names_sep = "_") %>%
+    mutate(pairs_1 = trimws(pairs_1), pairs_2 = trimws(pairs_2)) %>%  # Remove leading/trailing whitespaces
+    rowwise() %>%
+    mutate(Author1 = min(pairs_1, pairs_2), Author2 = max(pairs_1, pairs_2)) %>%  # Order author pairs alphabetically
+    ungroup() %>%
+    distinct(Author1, Author2)  # Remove duplicate author pairs
+}
+
+get_network_description.Interactions <- function(interactions) {
+  interactions$directed <- is_directed(interactions$graph)
+  interactions$weighted <- is_weighted(interactions$graph)
+  return(interactions)
+}
+
+get_network_description <- function(interactions) {
+  UseMethod("get_network_description", interactions)
+}
+
+get_cohesion.Interactions <- function(interactions) {
+  interactions$dyadcount <- network.dyadcount(interactions$network) # How many dyads? (n*n-1)
+  interactions$edgecount <- network.edgecount(interactions$network) # How many edges?
+  interactions$size <- network.size(interactions$network) # How large is the network?
+  return(interactions)
+}
+
+get_cohesion <- function(interactions) {
+  UseMethod("get_cohesion", interactions)
+}
+
+get_density.Interactions <- function(interactions) {
+  return(gden(interactions$network))
+}
+
+get_density <- function(interactions) {
+  UseMethod("get_density", interactions)
+}
+
+get_transitivity.Interactions <- function(interactions) {
+  return(gtrans(interactions$network))
+}
+
+get_transitivity <- function(interactions) {
+  UseMethod("get_transitivity", interactions)
+}
+
+get_centrality.Interactions <- function(interactions) {
+  return(igraph::degree(interactions$graph))
+}
+
+get_centrality <- function(interactions) {
+  UseMethod("get_centrality", interactions)
+}
+
+get_betweenness.Interactions <- function(interactions) {
+  return(igraph::betweenness(interactions$graph))
+}
+
+get_betweenness <- function(interactions) {
+  UseMethod("get_betweenness", interactions)
+}
+
+get_closeness.Interactions <- function(interactions) {
+  return(igraph::closeness(interactions$graph))
+}
+
+get_closeness <- function(interactions) {
+  UseMethod("get_closeness", interactions)
+}
+
+get_diameter.Interactions <- function(interactions) {
+  return(igraph::diameter(interactions$graph, directed = FALSE, weights = NA))
+}
+
+get_diameter <- function(interactions) {
+  UseMethod("get_diameter", interactions)
+}
