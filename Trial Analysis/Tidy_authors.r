ed0f744f5c1ed41ba5cce1d3411df40db0de9a00
DanielThedie <dthedie@gmail.com>
2025-02-27 10:23:11 +0000

    Add space after semicolon

--- Trial Analysis/Tidy_authors.r
+++ Trial Analysis/Tidy_authors.r
@@ -4,4 +4,8 @@ data <- import_csv_data("Trial Analysis\\Pure_23_24_Code.csv")
 
 data <- tidy_authors(data, author_column = "Author")
 
+author_col <- rlang::sym("Author")
+
+data <- data %>% mutate(!!author_col := stringr::str_replace_all(!!author_col, ";", "; "))
+
 write.csv(data, "Trial Analysis\\Pure_23_24_Code_tidy_authors.csv", row.names = FALSE)
