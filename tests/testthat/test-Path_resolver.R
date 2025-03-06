e78c11fe5282ec657891029a63c85e3929c9a168
DanielThedie <dthedie@gmail.com>
2025-03-06 16:19:00 +0000

    Fix Path resolver tests

--- tests/testthat/test-Path_resolver.R
+++ tests/testthat/test-Path_resolver.R
@@ -4,49 +4,37 @@ config <- list(
   output_path = tempdir()
 )
 
-test_that("Paths function works correctly", {
-  paths <- Paths(config)
-
-  expect_equal(paths$input_file, config$file_path)
-  expect_equal(paths$output, paste0(config$output_path, "/input_file"))
-  expect_equal(paths$figures, paste0(config$output_path, "/input_file/figures"))
-  expect_equal(paths$summary_table, paste0(config$output_path, "/input_file/data/Summary_statistics.csv"))
-  expect_s3_class(paths, "Paths")
-})
-
-test_that("create_output_paths function works correctly", {
-  paths <- create_output_paths(config)
-
-  expect_true(dir.exists(paths$output))
-  expect_true(dir.exists(paths$figures))
-})
-
 test_that("get_output_path function works correctly", {
+  stub(get_output_path, "getwd", function() "")
   output_path <- get_output_path(config)
 
-  expect_equal(output_path, paste0(config$output_path, "/input_file"))
+  expect_equal(output_path, paste0("/", config$output_path, "/input_file"))
 })
 
 test_that("get_figures_path function works correctly", {
+  stub(get_figures_path, "getwd", function() "")
   figures_path <- get_figures_path(config)
 
-  expect_equal(figures_path, paste0(config$output_path, "/input_file/figures"))
+  expect_equal(figures_path, paste0("/", config$output_path, "/input_file/figures"))
 })
 
 test_that("get_summary_table_path function works correctly", {
+  stub(get_summary_table_path, "getwd", function() "")
   summary_table_path <- get_summary_table_path(config)
 
-  expect_equal(summary_table_path, paste0(config$output_path, "/input_file/data/Summary_statistics.csv"))
+  expect_equal(summary_table_path, paste0("/", config$output_path, "/input_file/data/Summary_statistics.csv"))
 })
 
 test_that("get_data_path function works correctly", {
+  stub(get_data_path, "getwd", function() "")
   data_path <- get_data_path(config)
 
-  expect_equal(data_path, paste0(config$output_path, "/input_file/data"))
+  expect_equal(data_path, paste0("/", config$output_path, "/input_file/data"))
 })
 
 test_that("get_centrality_data_path function works correctly", {
+  stub(get_centrality_data_path, "getwd", function() "")
   centrality_data_path <- get_centrality_data_path(config)
 
-  expect_equal(centrality_data_path, paste0(config$output_path, "/input_file/data/Centrality_data_"))
+  expect_equal(centrality_data_path, paste0("/", config$output_path, "/input_file/data/Centrality_data_"))
 })
