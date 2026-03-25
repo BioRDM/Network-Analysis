e19ecea0aa73959d1b25a581c634292771aaf9e4
DanielThedie <dthedie@gmail.com>
2026-03-25 14:03:08 +0000

    Added a function to generate a new yaml config
    Function can be called with argument:
    * No argument: SNA_config.yaml created in current directory
    * Path to an existing directory: SNA_config.yaml created in target
    directory
    * Full path to a yaml config file: creates the file at the specified
    path (the file doesn't need to exist already, but the path does)

---
+++ R/Generate_config.r
@@ -0,0 +1,60 @@
+.default_config <- "metadata:
+  Author: Name of the report author
+  Email: Email address for the report author
+  Data description: Main data file description
+  Data access_date: 01-01-2020
+  Data source:
+  Data source_url:
+
+data:
+  file_path: /path/to/SBS_2019-2024_2025-03-24.csv
+  output_path: /path/to/Outputs
+  node_id: Name
+  node_delimiter:
+  edge_id: Title
+  year_column: Earliest published date
+  filters:
+    - Type == 'Article'
+  max_authors_per_paper:
+  min_papers_per_author:
+  from_year: 2019
+  to_year: 2024
+  split_per_year:
+
+plot:
+  layout: centrality  # centrality or auto
+
+node_properties:
+  file_path: /path/to/SBS_BPS_affiliations_tidy.csv
+  filters:
+    -
+  remove_NA: FALSE
+  node_id: Full.name
+  color: Department
+  order:
+    -
+  palette:
+    -
+"
+
+#' Generate a config file for the Network Analysis pipeline
+#'
+#' @param path The path where the config file will be created
+#' @returns None (writes a .yaml config file to the specified path)
+#' @export
+generate_config <- function(path = ".") {
+  # Add check that provided path exists
+  if (dir.exists(path)) {
+    out_file <- file.path(path, "SNA_config.yaml")
+  } else {
+    parent_dir <- dirname(path)
+
+    if (!dir.exists(parent_dir)) {
+      cli::cli_abort(c("x" = "Input path does not exist: parent directory is missing."))
+    }
+
+    out_file <- path
+  }
+
+  writeLines(.default_config, out_file)
+}
